13aa9b35e663ce884420afe0d6991236f846c889
glepnir <glepnir@gopherhub.org>
2020-07-02 22:56:38 +0800

    update

--- DataStructures/Chapter1-Linear/03-Queues/example_01/main.go
+++ DataStructures/Chapter1-Linear/03-Queues/example_01/main.go
@@ -37,6 +37,14 @@ func (q *Queue) Add(order *Order) {
 		appended := false
 		for key, addedOrder := range *q {
 			if order.priority > addedOrder.priority {
+				// 其他代码没什么解释的这一行新手可能看不懂解释一下
+				// 比较已添加订单和准备添加订单的优先级
+				// 如果准备添加的订单优先级比已经添加的订单的优先级高那么
+				// 先生成一个新的订单队列切片元素是准备添加的订单Queue{order}
+				// 然后将从key开始的这个订单到最后的订单追加到这个新生成的订单队列
+				// 也就是这句代码(*q)[key:]... 三点运算符展开这个切片。
+				// 再讲这个新的订单队列切片展开追加到原始切片到key的前面
+				// 再通俗一点就是在key的这个位置前面插入了一个元素也就是新订单。
 				*q = append((*q)[:key], append(Queue{order}, (*q)[key:]...)...)
 				appended = true
 				break
