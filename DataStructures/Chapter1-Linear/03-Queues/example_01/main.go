b705247f8b12c41c34ff31081e1fac840ff35420
glepnir <glepnir@gopherhub.org>
2020-07-02 22:21:00 +0800

    update

--- DataStructures/Chapter1-Linear/03-Queues/example_01/main.go
+++ DataStructures/Chapter1-Linear/03-Queues/example_01/main.go
@@ -7,6 +7,8 @@
 
 package main
 
+import "fmt"
+
 // 订单结构体
 type Order struct {
 	// 优先级
@@ -43,5 +45,39 @@ func (q *Queue) Add(order *Order) {
 		if !appended {
 			*q = append(*q, order)
 		}
+	}
+}
+
+// main method
+func main() {
+
+	var queue Queue
+	queue = make(Queue, 0)
+
+	var order1 *Order = &Order{}
+
+	var priority1 int = 2
+	var quantity1 int = 20
+	var product1 string = "Computer"
+	var customerName1 string = "Greg White"
+
+	order1.NewOrder(priority1, quantity1, product1, customerName1)
+
+	var order2 *Order = &Order{}
+
+	var priority2 int = 1
+	var quantity2 int = 10
+	var product2 string = "Monitor"
+	var customerName2 string = "John Smith"
+
+	order2.NewOrder(priority2, quantity2, product2, customerName2)
+
+	queue.Add(order1)
+
+	queue.Add(order2)
+
+	var i int
+	for i = 0; i < len(queue); i++ {
+		fmt.Println(queue[i])
 	}
 }
