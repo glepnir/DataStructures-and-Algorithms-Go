fad94b5309ea38a513efa4f86bbea371b2c1b2b0
Raphael <glepnir@gopherhub.org>
2020-07-07 22:59:36 +0800

    update

---
+++ DataStructures/Chapter1-Linear/04-Stack/stack/stack_test.go
@@ -0,0 +1,27 @@
+// Package main provides ...
+package main
+
+import "testing"
+
+var stack *ItemStack
+
+func init() {
+	stack = NewStack()
+}
+
+func Benchmark_Push(b *testing.B) {
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		stack.Push("test")
+	}
+}
+
+func Benchmark_Pop(b *testing.B) {
+	b.StopTimer()
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		stack.Push("test")
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		stack.Pop()
+	}
+}
