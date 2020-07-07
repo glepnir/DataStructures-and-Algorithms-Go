15cac1633982151a3e18be3c9e0e155f76e9c4a6
Raphael <glepnir@gopherhub.org>
2020-07-08 00:08:07 +0800

    update

---
+++ DataStructures/Chapter2-Non-Linear/Trees/code/binarysearchtree.go
@@ -0,0 +1,16 @@
+// Package main provides ...
+package main
+
+import "sync"
+
+type TreeNode struct {
+	key       int
+	value     int
+	leftNode  *TreeNode
+	rightNode *TreeNode
+}
+
+type BinarySearchTree struct {
+	rootNode *TreeNode
+	lock     sync.RWMutex
+}
